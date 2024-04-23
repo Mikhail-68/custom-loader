a39fa5568e05e311b1d76b3ff54435a3265aec50
Egorov Mikhail <misha2003200@gmail.com>
2024-04-23 09:09:32 +0300

    Init SpringBoot

---
+++ src/main/java/ru/mts/customloader/CustomLoaderApplication.java
@@ -0,0 +1,13 @@
+package ru.mts.customloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mts/customloader/CustomLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mts.customloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
